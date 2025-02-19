cafa8289b2ca3f36b3b0e0cf652b548d80e242b1
Gabriel Gonzalez Castane <lebriag@gmail.com>
2025-02-19 10:42:21 +0000

    code commit

---
+++ Debug/src/gs.d
@@ -0,0 +1,10 @@
+src/gs.o: ../src/gs.cpp \
+ /home/gcastane/Work/eclipseJavaWorkspace/clsim_g/include/gs.h \
+ /home/gcastane/Work/eclipseJavaWorkspace/clsim_g/include/inputs.h \
+ /home/gcastane/Work/eclipseJavaWorkspace/clsim_g/include/stat.h
+
+/home/gcastane/Work/eclipseJavaWorkspace/clsim_g/include/gs.h:
+
+/home/gcastane/Work/eclipseJavaWorkspace/clsim_g/include/inputs.h:
+
+/home/gcastane/Work/eclipseJavaWorkspace/clsim_g/include/stat.h:
---
+++ Debug/src/resource/power.d
@@ -0,0 +1,7 @@
+src/resource/power.o: ../src/resource/power.cpp \
+ /home/gcastane/Work/eclipseJavaWorkspace/clsim_g/include/inputs.h \
+ /home/gcastane/Work/eclipseJavaWorkspace/clsim_g/include/power.h
+
+/home/gcastane/Work/eclipseJavaWorkspace/clsim_g/include/inputs.h:
+
+/home/gcastane/Work/eclipseJavaWorkspace/clsim_g/include/power.h:
